a101fe0a77a193d5a6e55661d91061e9ee5272fb
Keiichi Hoshina <masayukihoshina3@gmail.com>
2021-08-04 00:41:33 +0900

    46. 再レンダリングと副作用を知る(useEffect)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+  // }, [num, faceShowFlag]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
@@ -13,7 +32,10 @@ const App = () => {
       <ColorfulMessage color="pink">お元気です！</ColorfulMessage>
       {/* <ColorfulMessage color="pink" message="お元気です！" /> */}
       <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>（＾_＾）</p>}
     </>
   );
 };
